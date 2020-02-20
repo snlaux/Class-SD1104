2e2c8f809a4abe991292d0b48cb6460299658bb8
slaux <slaux@EC2AMAZ-E7AQOGM>
2020-02-20 16:11:07 -0600

    update

---
+++ Slaux_Lab_4_6/Slaux_Lab_4_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Slaux_Lab_4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] day = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] task = new string[7];
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.WriteLine("What do you need to do on " + day[i] + "?");
+                task[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Your schedule...");
+            for (int i = 0;i <= 6; i++)
+            {
+                Console.WriteLine(day[i] + " : " + task[i]);
+            }
+        }
+    }
+}
