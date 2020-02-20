425af55d7240c9c3845ece730221d08adfb03888
slaux <slaux@EC2AMAZ-E7AQOGM>
2020-02-20 16:36:11 -0600

    update

---
+++ Slaux_Lab_5_2/Slaux_Lab_5_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Slaux_Lab_5_2
+{
+    class Program
+    {
+        public enum weekdays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            List<string> tasklist = new List<string>();
+            foreach(weekdays day in Enum.GetValues(typeof(weekdays)))
+            {
+                Console.WriteLine("Enter a task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                tasklist.Add(task);
+            }
+            foreach(string task in tasklist)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
