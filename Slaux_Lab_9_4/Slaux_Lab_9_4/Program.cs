1f559f5c3b8dd02cd277bf83046741946545e1d3
slaux <slaux@EC2AMAZ-E7AQOGM>
2020-03-12 16:34:04 -0500

    Added a variable list. Meaning we get how many items from the user. Instead of set 3 items.

--- Slaux_Lab_9_4/Slaux_Lab_9_4/Program.cs
+++ Slaux_Lab_9_4/Slaux_Lab_9_4/Program.cs
@@ -9,26 +9,39 @@ namespace Slaux_Lab_9_4
     {
         class FileWriter
         {
-            private const string path = "D:\\Generated HTML\\myHTML.html";
+            //Sets path for HTML file
+            private const string path = "D:\\Generated HTML\\myHTML.html"; 
 
 
             public void CreatHTML(StringBuilder data)
             {
-                File.AppendAllText(path, data.ToString());
+                File.Delete(path); //Delete because I want to run multiple times
+                File.AppendAllText(path, data.ToString()); //takes a path and data and adds it to the file
+                //convert to string since we pass it a stringbuilder
             }
         }
         static void Main(string[] args)
         {
+            //Getting website title from user
             Console.WriteLine("Enter a title for the website.");
             string dynamicTitle = Console.ReadLine();
-            Console.WriteLine("Enter the first item in the list.");
-            string dynamicList1 = "<li>" + Console.ReadLine() + "</li>";
-            Console.WriteLine("Enter the second item in the list.");
-            string dynamicList2 = "<li>" + Console.ReadLine() + "</li>";
-            Console.WriteLine("Enter the last item in the list.");
-            string dynamicList3 = "<li>"+ Console.ReadLine()+"</li>";
 
-            StringBuilder html = new StringBuilder();
+            //getting how many itmes in list from user
+            Console.WriteLine("How many items in your list?");
+            int numOfItems = Convert.ToInt32(Console.ReadLine());
+            
+            StringBuilder dynamicList = new StringBuilder(); //stringbuilder for dynamic list
+
+            for(int i = 0; i < numOfItems; i++) //I wanted the user to be able to decide how many items they needed
+            {
+                //loops through and adds the items onto dynamic list one at a time
+                dynamicList.Append("<li>");
+                Console.WriteLine("Enter the next item in your list");
+                dynamicList.Append(Console.ReadLine());
+                dynamicList.Append("</li>");
+            }
+
+            //All the strings needed to build HTML code
             string titleOpen = "<h1>";
             string titleClose = "</h1>";
             string bodyOpen = "<body>";
@@ -36,18 +49,22 @@ namespace Slaux_Lab_9_4
             string ulOpen = "<ul>";
             string ulClose = "</ul>";
 
+            //stringbuilder to add all HTML code to
+            StringBuilder html = new StringBuilder();
+           
+            //Adding each item to HTML in order to form proper HTML code
             html.Append(bodyOpen);
             html.Append(titleOpen);
             html.Append(dynamicTitle);
             html.Append(titleClose);
             html.Append(ulOpen);
-            html.Append(dynamicList1);
-            html.Append(dynamicList2);
-            html.Append(dynamicList3);
+            html.Append(dynamicList);
             html.Append(ulClose);
             html.Append(bodyClose);
 
+            //Creates the object file to create store the code
             FileWriter file = new FileWriter();
+            //calls the createHTML method and passes it our HTML
             file.CreatHTML(html);
         }
     }
